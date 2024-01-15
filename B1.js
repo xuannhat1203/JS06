c34d825c79d79ccdc49ba3e9bae086c73786a171
xuannhat1203 <141288976+xuannhat1203@users.noreply.github.com>
2024-01-15 16:12:46 +0700

    Add files via upload

--- B1.js
+++ B1.js
@@ -1,9 +1,12 @@
-let a =+prompt("Nhap so a");
+let a = +prompt("Nhap so a");
 let sum = 0;
-for(let i = 1; i<=a;i++){
-    console.log(i);
-    if(i%2 !=0){
-        sum+=i;
+let count = 1;
+let max = 0;
+for (let i = 1; i < a; i++) {
+    sum += i;
+    if (sum % 2 !== 0 && sum > maxSum) {
+        maxSum = sum;
+        count++;
     }
 }
-console.log("tong cac so le la:",sum);
+console.log(max);
